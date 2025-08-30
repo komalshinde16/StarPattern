ddc67ec4d5d45559b7f9f5c3092d8b7967b9446a
komalshinde26 <komalambadasshinde@email.com>
2025-08-30 18:59:50 +0530

    init

---
+++ src/patterns/pattern3.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=5 ; i++) {
+
+            for (int j = 5; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k =1; k<=i ; k++){
+                System.out.print('*');
+        }
+            System.out.println();
+
+        }
+    }
+}
+
